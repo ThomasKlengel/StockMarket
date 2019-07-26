3d8d3b6eef7af84519ac1dceb02198432d778bdb
Thomas Klengel <thomas.klengel@xenon-automation.com>
2019-07-26 10:03:04 +0200

    * added summaries to CollectionViewModel properties

--- StockMarket/ViewModels/Base/CollectionViewModel.cs
+++ StockMarket/ViewModels/Base/CollectionViewModel.cs
@@ -7,24 +7,42 @@ using System.Windows.Media;
 
 namespace StockMarket.ViewModels
 {
+    /// <summary>
+    /// a base class for share related collections
+    /// </summary>
     public abstract class CollectionViewModel: ViewModelBase
     {
+        /// <summary>
+        /// The price for the shares today
+        /// </summary>
         public abstract double SumNow
         { get; }
 
+        /// <summary>
+        /// The price for the shares at the day bought
+        /// </summary>
         public abstract double SumBuy
         { get; }
 
+        /// <summary>
+        /// The amount of bought shares
+        /// </summary>
         public abstract int Amount
         {
             get; set;
         }
 
+        /// <summary>
+        /// The amount of sold shares
+        /// </summary>
         public abstract int AmountSold
         {
             get; 
         }
 
+        /// <summary>
+        /// The price difference between buy and today
+        /// </summary>
         public double Difference
         {
             get
@@ -33,6 +51,9 @@ namespace StockMarket.ViewModels
             }
         }
 
+        /// <summary>
+        /// The background for a share 
+        /// </summary>
         public Brush Background
         {
             get
@@ -49,6 +70,9 @@ namespace StockMarket.ViewModels
             }
         }
 
+        /// <summary>
+        /// The lost/gained percentage
+        /// </summary>
         public double Percentage
         {
             get { return SumNow / SumBuy - 1.0; }
